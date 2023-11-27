8c2f53818d7c08d228bf07d92f7d284ee4c01f25
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-27 12:28:21 +0100

    granted authority adjustment

--- BackEnd/Capstone/src/main/java/KlajdiNdoci/Capstone/entities/User.java
+++ BackEnd/Capstone/src/main/java/KlajdiNdoci/Capstone/entities/User.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -44,26 +45,26 @@ public class User implements UserDetails {
     private List<Comment> comments;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
