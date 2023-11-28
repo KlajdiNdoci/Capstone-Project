0b733eda462bbfd626cd89bbcfbcee03dc52ad42
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-28 15:02:26 +0100

    added user controller

---
+++ src/main/java/KlajdiNdoci/Capstone/controllers/UserController.java
@@ -0,0 +1,93 @@
+package KlajdiNdoci.Capstone.controllers;
+
+import KlajdiNdoci.Capstone.entities.User;
+import KlajdiNdoci.Capstone.exceptions.BadRequestException;
+import KlajdiNdoci.Capstone.payloads.NewUserDTO;
+import KlajdiNdoci.Capstone.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/USER")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("")
+    public Page<User> getUser(@RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "10") int size,
+                              @RequestParam(defaultValue = "id") String orderBy) {
+        return userService.findAll(page, size > 20 ? 5 : size, orderBy);
+    }
+
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable UUID id) {
+        return userService.findUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable UUID id) {
+        userService.findByIdAndDelete(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public User findByIdAndUpdate(@PathVariable UUID id, @RequestBody @Validated NewUserDTO body, BindingResult validation) {
+        if (validation.hasErrors()) {
+            throw new BadRequestException(validation.getAllErrors());
+        } else {
+            return userService.findByIdAndUpdate(id, body);
+        }
+    }
+
+
+    @GetMapping("/me")
+    public UserDetails getProfile(@AuthenticationPrincipal UserDetails currentUser) {
+        return currentUser;
+    }
+
+    @PutMapping("/me")
+    public UserDetails updateProfile(@AuthenticationPrincipal User currentUser, @RequestBody NewUserDTO body) {
+        return userService.findByIdAndUpdate(currentUser.getId(), body);
+    }
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProfile(@AuthenticationPrincipal User currentUser) {
+        userService.findByIdAndDelete(currentUser.getId());
+    }
+
+    @PutMapping("/{id}/upload")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public User upload(@RequestParam("avatar") MultipartFile body, @PathVariable UUID id) throws IOException {
+        try {
+            return userService.uploadImg(body, id);
+        }catch (Exception e){
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
+    @PutMapping("/me/upload")
+    public UserDetails uploadOnProfile(@AuthenticationPrincipal User currentUser,  @RequestParam("avatar") MultipartFile body) throws IOException {
+        try {
+            return userService.uploadImg(body, currentUser.getId());
+        }catch (Exception e){
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+}
