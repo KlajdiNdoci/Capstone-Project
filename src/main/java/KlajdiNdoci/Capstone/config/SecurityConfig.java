a4d9218211bf219242ce2f22ba6f8aab4153f7a7
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-12-21 17:41:44 +0100

    test

--- src/main/java/KlajdiNdoci/Capstone/config/SecurityConfig.java
+++ src/main/java/KlajdiNdoci/Capstone/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://65830e1eb8d6352cddd201d7--moonlit-treacle-8399d5.netlify.app"));
+        config.setAllowedOrigins(List.of("https://65830e1eb8d6352cddd201d7--moonlit-treacle-8399d5.netlify.app"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
