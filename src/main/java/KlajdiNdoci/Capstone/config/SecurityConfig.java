e73af87d4ef1a18a190916f443bd0e88977ccb20
Klajdi Ndoci <Klajdindoci@gmail.com>
2024-01-08 12:20:58 +0100

    test

--- src/main/java/KlajdiNdoci/Capstone/config/SecurityConfig.java
+++ src/main/java/KlajdiNdoci/Capstone/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://659bcc2202b53d3838980c3f--remarkable-wisp-e5e85a.netlify.app"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
